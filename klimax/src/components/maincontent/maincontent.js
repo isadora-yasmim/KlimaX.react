d1fe04a0bfd49b9730d664eb1af6c2a29d9e08f9
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-11-17 00:26:36 -0300

    Atualização
    Adição do conteúdo da página e da search bar

--- klimax/src/components/maincontent/maincontent.js
+++ klimax/src/components/maincontent/maincontent.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Title from "./title/title";
+import Description from "./description/description";
+import SearchBar from "./searchbar/searchbar";
 
 function Maincontent() {
     return (
       <div className="Maincontent">
        <Title/>
+       <SearchBar/>
+       <Description/>
       </div>
     );
   }
