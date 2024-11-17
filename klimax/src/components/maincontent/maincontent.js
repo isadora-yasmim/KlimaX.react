222992653806df7ce437b1eaaf5956b4f2665c48
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-11-16 23:10:30 -0300

    Atualizacao
    Adicionada a estrutura base das pastas e arquivos e progresso nos componentes

---
+++ klimax/src/components/maincontent/maincontent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./title/title";
+
+function Maincontent() {
+    return (
+      <div className="Maincontent">
+       <Title/>
+      </div>
+    );
+  }
+  
+  export default Maincontent;
